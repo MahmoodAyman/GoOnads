52066731beba00a0750245aeafc50cd5e8ccad75
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-08-14 00:50:13 +0200

    end reviews section

--- scripts/app.js
+++ scripts/app.js
@@ -25,9 +25,118 @@ window.addEventListener("scroll", function () {
     let scrollBottom = scrollTop + reveals[i].clientHeight;
     if (scrollTop <= windowHeight) {
       reveals[i].classList.add("reveal-active");
-    }
-    else {
+    } else {
       reveals[i].classList.remove("reveal-active");
     }
   }
 });
+
+// reviews slider
+// vars
+("use strict");
+var testim = document.getElementById("reviews__slider"),
+  reviewDot = Array.prototype.slice.call(
+    document.getElementById("slider-dots").children
+  ),
+  reviewContent = Array.prototype.slice.call(
+    document.getElementById("review-content").children
+  ),
+  testimLeftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  testimSpeed = 4500,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer,
+  touchStartPos,
+  touchEndPos,
+  touchPosDiff,
+  ignoreTouch = 30;
+window.onload = function () {
+  // Testim Script
+  function playSlide(slide) {
+    for (var k = 0; k < reviewDot.length; k++) {
+      reviewContent[k].classList.remove("active");
+      reviewContent[k].classList.remove("inactive");
+      reviewDot[k].classList.remove("active");
+    }
+
+    if (slide < 0) {
+      slide = currentSlide = reviewContent.length - 1;
+    }
+
+    if (slide > reviewContent.length - 1) {
+      slide = currentSlide = 0;
+    }
+
+    if (currentActive != currentSlide) {
+      reviewContent[currentActive].classList.add("inactive");
+    }
+    reviewContent[slide].classList.add("active");
+    reviewDot[slide].classList.add("active");
+
+    currentActive = currentSlide;
+
+    clearTimeout(testimTimer);
+    testimTimer = setTimeout(function () {
+      playSlide((currentSlide += 1));
+    }, testimSpeed);
+  }
+
+  testimLeftArrow.addEventListener("click", function () {
+    playSlide((currentSlide -= 1));
+  });
+
+  testimRightArrow.addEventListener("click", function () {
+    playSlide((currentSlide += 1));
+  });
+
+  for (var l = 0; l < reviewDot.length; l++) {
+    reviewDot[l].addEventListener("click", function () {
+      playSlide((currentSlide = reviewDot.indexOf(this)));
+    });
+  }
+
+  playSlide(currentSlide);
+
+  // keyboard shortcuts
+  document.addEventListener("keyup", function (e) {
+    switch (e.keyCode) {
+      case 37:
+        testimLeftArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  testim.addEventListener("touchstart", function (e) {
+    touchStartPos = e.changedTouches[0].clientX;
+  });
+
+  testim.addEventListener("touchend", function (e) {
+    touchEndPos = e.changedTouches[0].clientX;
+
+    touchPosDiff = touchStartPos - touchEndPos;
+
+    console.log(touchPosDiff);
+    console.log(touchStartPos);
+    console.log(touchEndPos);
+
+    if (touchPosDiff > 0 + ignoreTouch) {
+      testimLeftArrow.click();
+    } else if (touchPosDiff < 0 - ignoreTouch) {
+      testimRightArrow.click();
+    } else {
+      return;
+    }
+  });
+};
