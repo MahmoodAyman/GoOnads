f26de6c685e93ca23c442772075b96ab88f435be
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-08-13 18:59:59 +0200

    reveal animation for slogan

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,33 @@
 const navbarToggler = document.querySelector(".navbar-toggler");
 
-
 // navbar toggler
-const header= document.getElementById("header");
+const header = document.getElementById("header");
 navbarToggler.addEventListener("click", function () {
   this.firstElementChild.classList.toggle("fa-times");
 });
-
 
 // navbar background change on scroll
 window.onscroll = function () {
   if (window.scrollY >= 60 || window.pageYOffset >= 60) {
     header.classList.add("navbar-scroll");
   } else {
-   header.classList.remove("navbar-scroll");
+    header.classList.remove("navbar-scroll");
   }
 };
+
+// scroll reveal
+
+window.addEventListener("scroll", function () {
+  var reveals = document.querySelectorAll(".reveals");
+  for (var i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let scrollTop = reveals[i].getBoundingClientRect().top;
+    let scrollBottom = scrollTop + reveals[i].clientHeight;
+    if (scrollTop <= windowHeight-150) {
+      reveals[i].classList.add("reveal-active");
+    }
+    else {
+      reveals[i].classList.remove("reveal-active");
+    }
+  }
+});
