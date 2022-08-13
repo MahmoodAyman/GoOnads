c28519c97074a0d89d08b6b13b006229846d2a70
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-08-13 15:33:28 +0200

    slogan

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,14 @@
 const navbarToggler = document.querySelector(".navbar-toggler");
 
+
+// navbar toggler
 const header= document.getElementById("header");
 navbarToggler.addEventListener("click", function () {
   this.firstElementChild.classList.toggle("fa-times");
 });
 
+
+// navbar background change on scroll
 window.onscroll = function () {
   if (window.scrollY >= 60 || window.pageYOffset >= 60) {
     header.classList.add("navbar-scroll");
